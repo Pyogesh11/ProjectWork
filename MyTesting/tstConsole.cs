ae5b82dfeafb5674de1bfcda2640faf5d2e3ffae
robjohnson123 <p2423554@my365.dmu.ac.uk>
2019-02-18 17:15:36 +0000

    Added classes

---
+++ MyTesting/tstConsole.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClassLibrary;
+
+namespace MyTesting
+{
+    [TestClass]
+    public class tstConsole
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsConsole AConsole = new clsConsole();
+            Assert.IsNotNull(AConsole);
+        }
+
+        [TestMethod]
+        public void ActivePropertyOK()
+        {
+            //instance of class we will create
+            clsConsole AConsole = new clsConsole();
+            //test data to assign to the property
+            Boolean TestData = true;
+            AConsole.Active = TestData;
+            Assert.AreEqual(AConsole.Active, TestData);
+        }
+
+        [TestMethod]
+        public void DateAddedPropertyOK()
+        {
+            clsConsole AConsole = new clsConsole();
+            //test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assigns data to property
+            AConsole.DateAdded = TestData;
+            Assert.AreEqual(AConsole.DateAdded, TestData);
+        }
+
+        public void ConsolePropertyOK()
+        {   //instance of class
+            clsConsole AConsole = new clsConsole();
+            //Console data to assign to property
+            string TestData = "Xbox";
+            //assigns data to the property
+            AConsole.Console = TestData;
+            //test to see if they ae the same
+            Assert.AreEqual(AConsole.Console, TestData);
+        }
+        public void ManufacturerPopertyOK()
+        {   //instance of class
+            clsConsole AConsole = new clsConsole();
+            //Console data to assign to property
+            string TestData = "Microsoft";
+            //assigns data to the property
+            AConsole.Manufacturer = TestData;
+            //test to see if they ae the same
+            Assert.AreEqual(AConsole.Manufacturer, TestData);
+        }
+        public void PricePropertyOK()
+        {   //instance of class
+            clsConsole AConsole = new clsConsole();
+            //Console data to assign to property
+            Int32 TestData = 500;
+            //assigns data to the property
+            AConsole.Price = TestData;
+            //test to see if they ae the same
+            Assert.AreEqual(AConsole.Price, TestData);
+        }
+        public void StockQuantityPropertyOK()
+        {   //instance of class
+            clsConsole AConsole = new clsConsole();
+            //Console data to assign to property
+            Int32 TestData = 25000;
+            //assigns data to the property
+            AConsole.Stock = TestData;
+            //test to see if they ae the same
+            Assert.AreEqual(AConsole.Stock, TestData);
+        }
+    }
+}
