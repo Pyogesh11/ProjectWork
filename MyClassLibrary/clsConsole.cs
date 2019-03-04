3bd7632605c7839901bee5a90d5e8f6da6ea111b
robjohnson123 <p2423554@my365.dmu.ac.uk>
2019-03-04 10:50:01 +0000

    Added Methods

--- MyClassLibrary/clsConsole.cs
+++ MyClassLibrary/clsConsole.cs
@@ -10,5 +10,17 @@ namespace MyClassLibrary
         public string Manufacturer { get; set; }
         public int Price { get; set; }
         public int Stock { get; set; }
+
+        public string Valid(string manufacturer)
+        {
+            if (Manufacturer != "")
+            {
+                return "";
+            }
+            else
+            {
+                return "This must not be blank";
+            }
+        }
     }
 }
