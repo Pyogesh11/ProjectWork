515dcd8f01d7e50b839dc3ee08d081b4eae2114d
NakedMango <Sonic19871@gmail.com>
2019-03-18 17:14:13 +0000

    i did sum work

---
+++ MyTesting/tstEmployeeCollection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClassLibrary;
+using System.Collections.Generic;
+
+namespace MyTesting
+{
+    [TestClass]
+    public class tstEmployeeCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsEmployeeCollection AllEmployees = new clsEmployeeCollection();
+            {
+                //test to see that it exists
+                Assert.IsNotNull(AllEmployees);
+            }
+
+            
+        }
+
+
+        [TestMethod]
+        public void EmployeeListOk()
+        {
+            clsEmployeeCollection AllEmployees = new clsEmployeeCollection();
+            //creating some test data to assign to the propeity
+            //in the case the data needs to be a list of objects
+            List<clsEmployee> TestList = new List<clsEmployee>();
+            //add an item to the list
+            //create the item of test data
+            clsEmployee TestItem = new clsEmployee();
+            // set its propities
+            TestItem.EmployeeNo = 1;
+            TestItem.EmployeeFirstName = "Tokigawa";
+            TestItem.EmployeeSurName = "Max";
+            TestItem.EmployeeContactNo = "07392738291";
+            TestItem.EmployeeEmail = "dingas@gmail.com.com";
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data ti the proepity
+            AllEmployees.EmployeeList = TestList;
+            //test ti see that the tw calues are the same
+            Assert.AreEqual(AllEmployees.EmployeeList, TestList); 
+
+
+
+        }
+    }
+
+}
