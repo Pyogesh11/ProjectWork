2928b34f7943f01587ed5fc8b9c60a4f616db5e7
P17195763 <P17195763@my365.dmu.ac.uk>
2019-03-19 17:20:01 +0000

    dadada

--- MyTesting/tstGamesCollection.cs
+++ MyTesting/tstGamesCollection.cs
@@ -50,7 +50,7 @@ namespace MyTesting
             //crete an instance of the class we want to create
             clsGamesCollection AllGames = new clsGamesCollection();
             //create some test data to assign to the property
-            Int32 SomeCount = 2;
+            Int32 SomeCount = 0;
             //assign the data to the property
             AllGames.Count = SomeCount;
             //test to see that the 2 values are the same 
