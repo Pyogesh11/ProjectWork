e03f74a99b55b580a9c839e102b6372778045c25
NakedMango <Sonic19871@gmail.com>
2019-03-19 13:00:04 +0000

    MORE WORK

--- MyTesting/tstEmployeeCollection.cs
+++ MyTesting/tstEmployeeCollection.cs
@@ -17,10 +17,10 @@ namespace MyTesting
                 Assert.IsNotNull(AllEmployees);
             }
 
-            
+
         }
 
-
+        // creating the employee list which will provide us an index ed list of employee frim the database tabel
         [TestMethod]
         public void EmployeeListOk()
         {
@@ -42,19 +42,68 @@ namespace MyTesting
             //assign the data ti the proepity
             AllEmployees.EmployeeList = TestList;
             //test ti see that the tw calues are the same
-            Assert.AreEqual(AllEmployees.EmployeeList, TestList); 
+            Assert.AreEqual(AllEmployees.EmployeeList, TestList);
 
 
+
+        }
+
+        // the following code is for the creation of the count propeity in the class clsEmployeeCollection
+        //this will alwasy tell us how many address there are in the list
+        [TestMethod]
+        public void CountEmployeeOK()
+        {
+            // creating an instance of the class clsEmployeeCollection
+            clsEmployeeCollection AllEmployee = new clsEmployeeCollection();
+            // creating some test data yo assign a propeity
+            Int32 SomeCount = 0;
+            //assign the data to the proepity
+            AllEmployee.Count = SomeCount;
+            Assert.AreEqual(AllEmployee.Count, SomeCount);
 
         }
 
 
         [TestMethod]
-        public void CountEmployeeOK()
+        public void ThisEmployeeOK()
         {
-            // creating an instance of the class clsEmployeeCollection
-            //dadeda
+            //creating an instance of the class clsEmployeeCollection
+            clsEmployeeCollection AllEmployee = new clsEmployeeCollection();
+            clsEmployee TestEmployee = new clsEmployee();
+            //Set the propeities of the test object
+            TestEmployee.EmployeeNo = 1;
+            TestEmployee.EmployeeFirstName = "Max";
+            TestEmployee.EmployeeSurName = "Tokigawa";
+            TestEmployee.EmployeeContactNo = "07392738291";
+            TestEmployee.EmployeeEmail = "dingas@gmail.com";
+            AllEmployee.ThisEmployee = TestEmployee;
+            Assert.AreEqual(AllEmployee.ThisEmployee, TestEmployee);
         }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //CREATING AN INSTANCE OF THE CLASS WE WANT TO CREATE
+            clsEmployeeCollection AllEmployee = new clsEmployeeCollection();
+            List<clsEmployee> TestList = new List<clsEmployee>();
+            //add an item to the list
+            //create the item of test data
+            clsEmployee TestItem = new clsEmployee();
+            //aet its propeities
+            TestItem.EmployeeNo = 1;
+            TestItem.EmployeeFirstName = "Max";
+            TestItem.EmployeeSurName = "Tokigawa";
+            TestItem.EmployeeContactNo = "07392738291";
+            TestItem.EmployeeEmail = "dingas@gmail.com";
+            //add the item  to the test list
+            TestList.Add(TestItem);
+            AllEmployee.EmployeeList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllEmployee.Count, TestList.Count); 
+
+        }
+        
+
     }
 
 }
