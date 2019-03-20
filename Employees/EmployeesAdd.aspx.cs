91d90203c5853020d4a8932fdb950ffa8b10b235
NakedMango <Sonic19871@gmail.com>
2019-03-20 19:20:38 +0000

    did sum wok

---
+++ Employees/EmployeesAdd.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MyClassLibrary; 
+public partial class EmployeesAdd : System.Web.UI.Page
+{
+    
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    void Add()
+    {
+        //create an instance of the employee
+        clsEmployeeCollection EmployeeGroup = new clsEmployeeCollection();
+        //validate the data on the web form
+        string Error = EmployeeGroup.ThisEmployee.Valid(txtEAFA.Text, txtEAEmployeeSurName.Text, txtEAEmployeeContactNo.Text, txtEAEmployeeEmail.Text);
+        //if the data is ok then add it to the object
+        if (Error == "")
+        {
+            //get the data entered by the user
+            EmployeeGroup.ThisEmployee.EmployeeFirstName = txtEAFA.Text;
+            EmployeeGroup.ThisEmployee.EmployeeSurName = txtEAEmployeeSurName.Text;
+            EmployeeGroup.ThisEmployee.EmployeeContactNo = txtEAEmployeeContactNo.Text;
+            EmployeeGroup.ThisEmployee.EmployeeEmail = txtEAEmployeeEmail.Text;
+
+            EmployeeGroup.Add();
+        }
+        else
+        {
+            //REPOR AN ERROR
+            lblEAError.Text = "There wwhere problems with the data entered" + Error;
+        }
+    }
+
+    protected void btnUpdate_Click(object sender, EventArgs e)
+    {
+        Add();
+        //all done so redirect back to main page
+        Response.Redirect("EDefult.aspx");
+    }
+}
+
+//txtEAEmployeeFirstName
+//txtEAEmployeeSurName
+//txtEAEmployeeContactNo
+//txtEAEmplloyeeEmail
--- MyClassLibrary/clsEmployeeCollection.cs
+++ MyClassLibrary/clsEmployeeCollection.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace MyClassLibrary
 {
@@ -7,6 +8,26 @@ namespace MyClassLibrary
 
 
         List<clsEmployee> mEmployeeList = new List<clsEmployee>();
+        //private data member thisEmployee
+        clsEmployee mThisEmployee = new clsEmployee();
+
+
+        //PUBLIC PROP FOR ThisEmployee
+        public clsEmployee ThisEmployee
+        {
+            get
+            {
+                //return the privae data
+                return mThisEmployee;
+            }
+            set
+            {
+                //set the private data
+                mThisEmployee = value;
+            }
+        }
+
+
 
         public List<clsEmployee> EmployeeList
         {
@@ -40,7 +61,7 @@ namespace MyClassLibrary
             }
         }
        
-        public clsEmployee ThisEmployee { get; set; }
+        
 
 
 
@@ -49,35 +70,70 @@ namespace MyClassLibrary
         {
 
             //var to store the index
-            
+            Int32  Index = 0;
+            // var tp stor ethe record count
+            Int32 RecordCount = 0;
+            //object for data connection
+            clsDataConnection DB = new clsDataConnection();
+            //exercute the stored proceduer
+            DB.Execute("sproc_tblEmployees_SelectAll");
+            //get the count of records
+            RecordCount = DB.Count;
+            //while there are records to be processed
+            while (Index < RecordCount)
+            {
+                clsEmployee TestItem = new clsEmployee();
+                //seting the prameters
+                TestItem.EmployeeNo = 6;
+                TestItem.EmployeeFirstName = "Crona";
+                TestItem.EmployeeSurName = "Maximus;";
+                TestItem.EmployeeContactNo = "07364736261";
+                TestItem.EmployeeEmail = "Crona@gmail.com";
+                //  add the item to the test list
+                mEmployeeList.Add(TestItem);
+                // seting the prameters
+                TestItem.EmployeeNo = 7;
+                TestItem.EmployeeFirstName = "Prita";
+                TestItem.EmployeeSurName = "Sokanji;";
+                TestItem.EmployeeContactNo = "07726389431";
+                TestItem.EmployeeEmail = "Crona@gmail.com";
+                //add the item to the test list
+                mEmployeeList.Add(TestItem);
+                //point at the next record
+                Index++;
+
+
+            }
 
 
 
 
 
-            clsEmployee TestItem = new clsEmployee();
-            //seting the prameters
-            TestItem.EmployeeNo = 6;
-            TestItem.EmployeeFirstName = "Crona";
-            TestItem.EmployeeSurName = "Maximus;";
-            TestItem.EmployeeContactNo = "07364736261";
-            TestItem.EmployeeEmail = "Crona@gmail.com";
-            //add the item to the test list
-            mEmployeeList.Add(TestItem);
-            //seting the prameters
-            TestItem.EmployeeNo = 7;
-            TestItem.EmployeeFirstName = "Prita";
-            TestItem.EmployeeSurName = "Sokanji;";
-            TestItem.EmployeeContactNo = "07726389431";
-            TestItem.EmployeeEmail = "Crona@gmail.com";
-            //add the item to the test list
-            mEmployeeList.Add(TestItem);
-
+           
         }
 
-        
+        public int Add()
+        {
+            //connect to the dataase
+            clsDataConnection DB = new clsDataConnection();
+            //set the paramters fpr the stored proceduer
+            DB.AddParameter("@EmployeeFirstName", mThisEmployee.EmployeeFirstName);
+            DB.AddParameter("@EmployeeSurNmae", mThisEmployee.EmployeeSurName);
+            DB.AddParameter("@EmployeeContactNo", mThisEmployee.EmployeeContactNo);
+            DB.AddParameter("EmployeeEmail", mThisEmployee.EmployeeEmail);
+            //exercute the querry retruning the primary key value
+            return DB.Execute("sproc_tblEmployees_Insert");
+        }
 
-
-
+        public void Delete()
+        {
+            //deleates the record ponted to by ThisEmployee
+            //commect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //setting the param for the stored proceder
+            DB.AddParameter("@EmployeeNo", mThisEmployee.EmployeeNo);
+            //EXERCUTE THE STORED PROCEDUER
+            DB.Execute("sproc_tblEmployess_Delete");
+        }
     }
 }
